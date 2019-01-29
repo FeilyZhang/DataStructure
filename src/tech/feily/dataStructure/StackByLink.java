2ef81f695be3cf3909a2d32da7bd0ea2b6ac4939
Feily Zhang <fei472625@foxmail.com>
2019-01-29 17:01:16 +0800

    增加栈的链表实现

---
+++ src/tech/feily/dataStructure/StackByLink.java
@@ -0,0 +1,35 @@
+package tech.feily.dataStructure;
+
+/**
+ * @author Administrator 
+ * @date : 2019年1月29日下午4:53:20
+ * @version : V1.0 
+ * @Description :  
+ * @Copyright : All rights Reserved, Designed By https://feily.tech.
+ */
+public class StackByLink {
+
+    private Link link = new Link();
+    
+    public void push(int data) {
+        link.addLast(data);
+    }
+    
+    public int peek(int data) {
+        return link.get(link.size() - 1).getData();
+    }
+    
+    public int pop() {
+        int temp = link.get(link.size() - 1).getData();
+        link.removeLast();
+        return temp;
+    }
+    
+    public boolean isEmpty() {
+        return link.size() == 0;
+    }
+    
+    public int size() {
+        return link.size();
+    }
+}
