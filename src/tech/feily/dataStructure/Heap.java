f9754a60d7bef69ee67ec3b0e9bcddf96cb8cf02
Feily Zhang <fei472625@foxmail.com>
2019-02-02 12:25:57 +0800

    堆的根节点删除方法

--- src/tech/feily/dataStructure/Heap.java
+++ src/tech/feily/dataStructure/Heap.java
@@ -53,6 +53,35 @@ public class Heap {
         System.out.println();
     }
     
+    public int delete() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("The big top heap is empty.");
+        }
+        int deleteElement = elements[1];    // 要删除的节点，那么根节点会为空
+        elements[1] = elements[elements[0]];    // 将数组尾部的值替换到根节点
+        elements[0] --; // 长度减一，因为最后一个节点已经在索引为1的位置，即最后一个节点已经是根节点
+        // 但是最后一个节点一般情况下不满足堆属性，接下来修正堆属性
+        // 先临时保存根节点，因为要修正堆属性，采取的方法类似于插入排序的置换方法
+        // 也就是说最终会空出来一个位置，这个位置就是temp节点的位置，直接填补上去即可
+        int temp = elements[1];
+        int parent = 1;
+        int child = 2;
+        while (child <= elements[0]) {
+            if (child < elements[0] && elements[child] < elements[child + 1]) {
+                child ++;
+            }
+            if (temp > elements[child]) {
+                break;
+            } else {
+                elements[parent] = elements[child];
+                parent = child;
+                child += 2;
+            }
+        }
+        elements[parent] = temp;
+        return deleteElement;
+    }
+    
     public static void main(String[] args) {
         Heap heap = new Heap(10);
         heap.insert(12);
@@ -60,6 +89,8 @@ public class Heap {
         heap.insert(6);
         heap.insert(35);
         heap.insert(58);
+        heap.printAll();
+        System.out.println(heap.delete());
         heap.printAll();
     }
 }
